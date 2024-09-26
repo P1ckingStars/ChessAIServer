cf1753df288a5d40b3aa2d148707ba9c236b7808
arthur <arthurwangzihao@gmail.com>
2024-09-25 22:50:10 -0400

    update

--- src/topology/chess_ai_compute_node.cpp
+++ src/topology/chess_ai_compute_node.cpp
@@ -1,4 +1,5 @@
 #include "bss.h"
+#include <cstdio>
 #include <stddef.h>
 /*
 compute: dequeue task, work on task -> return back to balancer node
@@ -8,18 +9,20 @@ client
 client: -> balancer
 */
 
-void process_task(BSS * bss, Task task) {
+void process_task(BSS *bss, Task task) {
   // TODO
-    
 }
 
 int dsm_main(char *mem_region, size_t length, int argc, char *argv[]) {
+  printf("----------Start Server----------");
+  printf("%lu\n", sizeof(BSS));
   BSS *bss = (BSS *)mem_region;
   char *base = mem_region + 4096;
 
   Task task;
   while (1) {
-    while (!bss->task_queue_ptr->pop(task));
+    while (!bss->task_queue_ptr->pop(task))
+      ;
     process_task(bss, task);
   }
 
